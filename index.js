c2557d79eaa581ed55a4528f12350781d0cd7040
cyberfunk <hello@cyberfunk.org>
2014-01-30 19:08:06 +0800

    Use ~ bitwise operator.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var short = '-', long = '--';
-var sre = /^-[^-]+/, lre = /^--[^-]+/, negate = /^--no-/;
+var sre = /^-[^-]+/, lre = /^--[^-]+/, negate = /--no-/;
 var camelcase = require('cli-util').camelcase;
 
 function toOptionKey(arg, negated, opts) {
@@ -14,22 +14,22 @@ function alias(key, opts) {
   var alias = opts.alias, z, keys;
   for(z in alias) {
     keys = z.split(/\s+/);
-    if(keys.indexOf(key) > -1) return {key: alias[z], aliased: true, negated: /--no-/.test(z)};
+    if(~keys.indexOf(key)) return {key: alias[z],
+      aliased: true, negated: negate.test(z)};
   }
   return {key: key, aliased: false};
 }
 
 function flags(arg, output, next, opts) {
   var result = alias(arg, opts), keys, skip = false, i = 0, key, v = true;
-  if(result.aliased) output.flags[result.key] = true;
+  if(result.aliased) output.flags[result.key] = v;
   arg = arg.replace(/^-/, ''); keys = arg.split('');
   for(;i < keys.length;i++, key = keys[i]) {
     key = keys[i];
-    if(i == keys.length - 1 && opts.options.indexOf(short + key) > -1) {
+    if(i == keys.length - 1 && ~opts.options.indexOf(short + key)) {
       return options(short + key, output, next, opts);
     }
     result = alias(short + key, opts);
-    // short option flag has an alias component that negates (--no-)
     if(result.negated) v = false;
     output.flags[result.aliased ? result.key : key] = v;
   }
@@ -38,13 +38,13 @@ function flags(arg, output, next, opts) {
 
 function options(arg, output, next, opts) {
   var equals = arg.indexOf('='), value, result = false, negated, key;
-  var flag = (!next && equals == -1)
-    || (next && next.indexOf(short) == 0 && equals == -1);
-  if(equals > -1) {
+  var flag = (!next && !~equals)
+    || (next && next.indexOf(short) == 0 && !~equals);
+  if(~equals) {
     value = arg.slice(equals + 1); arg = arg.slice(0, equals);
   }
-  if(opts.flags.indexOf(arg) > -1) flag = true;
-  if(next && !flag && equals == -1) {
+  if(~opts.flags.indexOf(arg)) flag = true;
+  if(next && !flag && !~equals) {
     value = next; result = true;
   }
   negated = negate.test(arg);
@@ -76,7 +76,7 @@ function parse(args, opts) {
     arg = '' + args.shift(), skip = false;
     larg = lre.test(arg);
     opts.options.forEach(function(o){
-      if(arg.indexOf(o) > -1) larg = true;
+      if(~arg.indexOf(o)) larg = true;
     });
     if(arg == short) {
       output.stdin = true;
