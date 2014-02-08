2f27d773f893aee5e446aa01a7306b2c9a4bfb72
cyberfunk <hello@cyberfunk.org>
2014-02-09 00:13:19 +0800

    Assert on empty string.

--- test/unit/equality.js
+++ test/unit/equality.js
@@ -2,24 +2,27 @@ var expect = require('chai').expect;
 var parse = require('../..');
 
 describe('cli-argparse:', function() {
-  it('should coerce missing value to empty string', function(done) {
+  it('should coerce missing value to empty string (short)', function(done) {
+    var args = ['-p='];
+    var result = parse(args);
+    expect(result.options.p).to.be.a('string').that.equals('');
+    done();
+  });
+  it('should coerce missing value to empty string (long)', function(done) {
     var args = ['--port='];
     var result = parse(args);
-    expect(result.raw).to.eql(args);
     expect(result.options.port).to.be.a('string').that.equals('');
     done();
   });
   it('should parse long option (equality)', function(done) {
     var args = ['--port=80'];
     var result = parse(args);
-    expect(result.raw).to.eql(args);
     expect(result.options.port).to.be.a('string').that.equals('80');
     done();
   });
   it('should parse short option (equality)', function(done) {
     var args = ['-p=80'];
     var result = parse(args);
-    expect(result.raw).to.eql(args);
     expect(result.options.p).to.be.a('string').that.equals('80');
     done();
   });
@@ -27,7 +30,6 @@ describe('cli-argparse:', function() {
     function(done) {
       var args = ['port=80'];
       var result = parse(args);
-      expect(result.raw).to.eql(args);
       expect(result.options.port).to.be.a('string').that.equals('80');
       done();
     }
@@ -36,7 +38,6 @@ describe('cli-argparse:', function() {
     function(done) {
       var args = ['--port', 'variable=value'];
       var result = parse(args);
-      expect(result.raw).to.eql(args);
       expect(result.options.port)
         .to.be.a('string').that.equals('variable=value');
       done();
@@ -46,7 +47,6 @@ describe('cli-argparse:', function() {
     function(done) {
       var args = ['--port=variable=value'];
       var result = parse(args);
-      expect(result.raw).to.eql(args);
       expect(result.options.port)
         .to.be.a('string').that.equals('variable=value');
       done();
@@ -57,7 +57,6 @@ describe('cli-argparse:', function() {
       var conf = {options: ['-p']}
       var args = ['-p','variable=value'];
       var result = parse(args, conf);
-      expect(result.raw).to.eql(args);
       expect(result.options.p)
         .to.be.a('string').that.equals('variable=value');
       done();
